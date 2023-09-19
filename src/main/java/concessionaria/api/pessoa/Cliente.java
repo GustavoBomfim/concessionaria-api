85a891e5e51d872ca46903f62571876f2e3fcf8e
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-09-19 20:02:52 -0300

    Criando as classes Pessoa e Cliente

---
+++ src/main/java/concessionaria/api/pessoa/Cliente.java
@@ -0,0 +1,9 @@
+package concessionaria.api.pessoa;
+
+public class Cliente {
+    private String cpfOuCnpj;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String compras;
+}
---
+++ src/main/java/concessionaria/api/pessoa/Funcionario.java
@@ -0,0 +1,18 @@
+package concessionaria.api.pessoa;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Funcionario {
+    private int id;
+    private String cpfCnpj;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String compras;
+    private BigDecimal salarioBase;
+    private double percentualComissao;
+    private String cargo;
+    private Date dataContratacao;
+
+}
