9fa7f490bf0eb106d6be4f0ed09aba91fd161754
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-09-19 20:09:22 -0300

    Criando as classes Modelo e Veiculo

---
+++ src/main/java/concessionaria/api/veiculo/Modelo.java
@@ -0,0 +1,13 @@
+package concessionaria.api.veiculo;
+
+public class Modelo {
+    private String marca;
+    private String cor;
+    private int anoFabricacao;
+    private int anoModelo;
+    private String combustivel;
+    private int portas;
+    private boolean tetoSolar;
+    private boolean arCondicionado;
+
+}
---
+++ src/main/java/concessionaria/api/veiculo/Veiculo.java
@@ -0,0 +1,9 @@
+package concessionaria.api.veiculo;
+
+public class Veiculo {
+    private int chassi;
+    private String placa;
+    private Boolean disponivel;
+    private Double preco;
+    private Modelo modelo;
+}
